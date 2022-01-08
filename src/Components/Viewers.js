19a022bd6aa28929abd9afcfd838f5d1aff7a50a
Carlos Escobar <escobarcarlos@gmail.com>
2022-01-07 17:19:09 -0800

    animationImages

--- src/Components/Viewers.js
+++ src/Components/Viewers.js
@@ -8,16 +8,16 @@ function Viewers() {
                 <img src="/images 2/viewers-disney.png"/>
             </Wrap>
             <Wrap>
-                <img src="/images 2/viewers-disney.png"/>
+                <img src="/images 2/viewers-pixar.png"/>
             </Wrap>
             <Wrap>
-                <img src="/images 2/viewers-disney.png"/>
+                <img src="/images 2/viewers-marvel.png"/>
             </Wrap>
             <Wrap>
-                <img src="/images 2/viewers-disney.png"/>
+                <img src="/images 2/viewers-starwars.png"/>
             </Wrap>
             <Wrap>
-                <img src="/images 2/viewers-disney.png"/>
+                <img src="/images 2/viewers-national.png"/>
             </Wrap>
         </Container>
     )
@@ -31,18 +31,31 @@ export default Viewers
 const Container = styled.div`
     margin-top: 30px; 
     display:grid; 
+    padding: 30px 0px 26px; 
+    grid-gap:25px;
     grid-template-columns: repeat(5, minmax(0, 1fr));
 
 `
 const Wrap = styled.div`
     border-radius: 10px;
     border: 3px solid rgba(249,249,249, 0.1 ); 
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px; 
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s; 
+
 
 
     img{
         width: 100%;
         height: 100%; 
         object-fit:cover;
+    }
+
+    &:hover { 
+        box-shadow: rgba(0 0 0 / 80%) 0px 40px 58px -16px,
+        rgba(0 0 0 / 72%) 0px 30px 22px -10px; 
+        transform: scale(1.05); 
+        border-color: rgba(249, 249, 249, 0.8)
     }
 
 
